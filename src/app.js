fc4a623780a3f8d06a6f0ea87560df09b2a3f58d
tuncmehmet <mehmettunc96@gmail.com>
2021-10-19 02:12:31 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,7 @@
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>");
+for(let i = 0; i < konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>");
+}
+console.log("</ul>");
